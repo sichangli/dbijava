be8dd0daf7eaa929c1833e777d2ec8b3ed273937
ALEX <Alex.XiaT@gmail.com>
2013-04-04 17:31:55 -0400

    test

--- src/op.java
+++ src/op.java
@@ -1,7 +1,26 @@
+import java.util.HashMap;
+
 
 public class OP {
 	
 	public static void main(String[] args) {
 		
 	}
+	
+	private double calFixCost(int n, HashMap<String, Double> map){
+		double r = map.get("r");
+		double l = map.get("l");
+		double t = map.get("t");
+		double f = map.get("f");
+		return n*r+(n-1)*l+n*f+t;
+	}
+	
+	private double[] calMetricC(int n, double p, HashMap<String, Double> map){
+		double fcost = calFixCost(n, map);
+		double[] rs = new double[2];
+		rs[0] = ((p-1)/fcost);
+		rs[1] = p;
+		return 
+	}
+	
 }
